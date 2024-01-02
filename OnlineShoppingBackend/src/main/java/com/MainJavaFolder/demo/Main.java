85d21b3e82dee59d21c30679480644b0d570e8e9
Narayan <nchuwan500@gmail.com>
2024-01-02 10:35:33 -0600

    Making server

--- OnlineShoppingBackend/src/main/java/com/MainJavaFolder/demo/Main.java
+++ OnlineShoppingBackend/src/main/java/com/MainJavaFolder/demo/Main.java
@@ -1,14 +1,14 @@
-package com.example.demo;
+package com.MainJavaFolder.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello");
+		SpringApplication.run(Main.class, args);
 	}
 
 }
